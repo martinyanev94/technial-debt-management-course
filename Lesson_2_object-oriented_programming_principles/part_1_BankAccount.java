9a3c8c371f2a220f543258c067911b1fef535fc6
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-30 20:04:14 -0400

    Add Lesson_2_object-oriented_programming_principles/part_1_BankAccount.java

---
+++ Lesson_2_object-oriented_programming_principles/part_1_BankAccount.java
@@ -0,0 +1,29 @@
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrew: " + amount);
+        } else {
+            System.out.println("Invalid withdrawal amount.");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}

ea08cb90460153dd35d401d49fd0d762ffbb0107
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-30 20:03:24 -0400

    Add Lesson_22_Visitor_Pattern_for_Object_Operations/part_3_ShapesImplementation.java

---
+++ Lesson_22_Visitor_Pattern_for_Object_Operations/part_3_ShapesImplementation.java
@@ -0,0 +1,56 @@
+class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public void accept(ShapeVisitor visitor) {
+        visitor.visit(this);
+    }
+}
+
+class Square implements Shape {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    @Override
+    public void accept(ShapeVisitor visitor) {
+        visitor.visit(this);
+    }
+}
+
+class Triangle implements Shape {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public void accept(ShapeVisitor visitor) {
+        visitor.visit(this);
+    }
+}

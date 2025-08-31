aaee942135f895fb5446f9562671bdae690046dc
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-30 20:04:03 -0400

    Add Lesson_28_Identifying_Common_Patterns/part_3_ObserverPattern.java

---
+++ Lesson_28_Identifying_Common_Patterns/part_3_ObserverPattern.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+interface Observer {
+    void update(String message);
+}
+
+class ConcreteObserver implements Observer {
+    private String name;
+
+    public ConcreteObserver(String name) {
+        this.name = name;
+    }
+
+    public void update(String message) {
+        System.out.println(name + ": " + message);
+    }
+}
+
+class Subject {
+    private List<Observer> observers = new ArrayList<>();
+
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void notifyObservers(String message) {
+        for (Observer observer : observers) {
+            observer.update(message);
+        }
+    }
+}

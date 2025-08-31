769c571b6609caf527d4a7872e0ad02930acec0d
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-30 20:03:52 -0400

    Add Lesson_27_Understanding_Anti_Patterns/part_3_vehicle_systems.java

---
+++ Lesson_27_Understanding_Anti_Patterns/part_3_vehicle_systems.java
@@ -0,0 +1,40 @@
+class VehicleSystem {
+    void drive() {
+        // Extensive logic for driving
+    }
+
+    void checkFuel() {
+        // Logic for checking fuel
+    }
+
+    void checkTires() {
+        // Logic for checking tires
+    }
+
+    void performMaintenance() {
+        // All maintenance actions encapsulated here
+    }
+}
+class Vehicle {
+    void drive() {
+        // Driving logic
+    }
+}
+
+class FuelChecker {
+    void checkFuel() {
+        // Fuel checking logic
+    }
+}
+
+class TireChecker {
+    void checkTires() {
+        // Tire checking logic
+    }
+}
+
+class MaintenanceService {
+    void performMaintenance() {
+        // Maintenance logic
+    }
+}

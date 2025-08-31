451590e4ac765bed99eb94b151de7682613804e4
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-30 20:04:00 -0400

    Add Lesson_28_Identifying_Common_Patterns/part_1_Singleton.java

---
+++ Lesson_28_Identifying_Common_Patterns/part_1_Singleton.java
@@ -0,0 +1,18 @@
+class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+        // private constructor to restrict instantiation
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public void showMessage() {
+        System.out.println("Hello from Singleton!");
+    }
+}

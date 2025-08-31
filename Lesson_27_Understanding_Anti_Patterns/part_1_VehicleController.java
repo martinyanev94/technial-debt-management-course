b336205ad80c4335d52e80147e16bef774d57886
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-30 20:03:49 -0400

    Add Lesson_27_Understanding_Anti_Patterns/part_1_VehicleController.java

---
+++ Lesson_27_Understanding_Anti_Patterns/part_1_VehicleController.java
@@ -0,0 +1,21 @@
+class Vehicle {
+    void drive() {
+        // Driving mechanism
+        // Engine control logic
+        // Brake checks
+        // Gear shifting
+    }
+
+    void checkFuel() {
+        // Fuel level check logic
+    }
+
+    void checkTires() {
+        // Tire pressure checking logic
+    }
+
+    void navigate() {
+        // Navigation logic and system control
+        // More complex conditional statements
+    }
+}
